22f7e03a7d724668bed1ef40e235eab08f7edba0
asdfka345 <vito.axel2001@gmail.com>
2025-05-12 23:57:05 +0700

    init commit

---
+++ src/main/java/com/juaracoding/axelvitoujian1jf/ujian1/Soal1.java
@@ -0,0 +1,15 @@
+package com.juaracoding.axelvitoujian1jf.ujian1;
+
+import java.util.Scanner;
+
+public class Soal1 {
+    public static void main(String[] args) {
+            String namaFilm = "Avengers: Endgame";
+            int hargaTiket = 50000;
+            int jumlahTiket = 3;
+
+            System.out.println("Nama Film: " + namaFilm);
+            System.out.println("Harga Tiket: " + hargaTiket);
+            System.out.println("Jumlah Tiket: " + jumlahTiket);
+        }
+    }
